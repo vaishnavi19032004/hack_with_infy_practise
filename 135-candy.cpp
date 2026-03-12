388c52bf041eac63615ba0bfaa84ad4c539bfe3f
vaishnavi19032004 <vaishnavi19032004@gmail.com>
2026-03-13 01:20:57 +0530

    Create 135-candy.cpp

---
+++ 135-candy.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> v(n,1);
+        for(int i=1; i<n; i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                v[i]=v[i-1]+1;
+            }
+        }
+
+        for(int i=n-2; i>=0; i--)
+        {
+            if(ratings[i]>ratings[i+1])
+            {
+                v[i]=max(v[i],v[i+1]+1);
+            }
+        }
+        int sum=0;
+        for(int c:v) sum = sum+c;
+
+        return sum;
+        
+    }
+};
